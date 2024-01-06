791520e001fe04f98f1f5ecf52bfc7114cba744a
Maicol Salazar <stivsal12@gmail.com>
2024-01-05 20:24:52 -0500

    mejoras de vistas

---
+++ javascrip.js
@@ -0,0 +1,7 @@
+const menuNavegacion = document.getElementById('navMenu')
+addEventListener
+const botonMenu = document.getElementById('btnOpen')
+
+function desplegarMenu (){
+    menuNavegacion.classList.toggle('show-menu')
+}
