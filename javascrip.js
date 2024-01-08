d3191319a4a7bb257017dc06e9fc2db6df1b3566
Maicol Salazar <stivsal12@gmail.com>
2024-01-08 18:18:53 -0500

    se agrego el icono para cambiar de modo oscuro a modo claro

--- javascrip.js
+++ javascrip.js
@@ -1,6 +1,8 @@
 const menuNavegacion = document.getElementById('navMenu')
 addEventListener
 const botonMenu = document.getElementById('btnOpen')
+const changeMode = document.getElementById('switch-light-dark')
+changeMode.addEventListener('click')
 
 function desplegarMenu (){
     menuNavegacion.classList.toggle('show-menu')
